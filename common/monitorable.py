34813c7335d92cd5c019ca66c897a1c99a216d79
Alan Botoshansky <91224532+AlanBotoshansky@users.noreply.github.com>
2025-06-09 18:23:09 -0300

    Add daemon to health checks receiver process
    Co-Authored-By: Ivan Botoshansky <91224546+IvanBotoshansky@users.noreply.github.com>
    Co-Authored-By: BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>

--- common/monitorable.py
+++ common/monitorable.py
@@ -4,7 +4,7 @@ from common.health_checks_receiver import HealthChecksReceiver
 
 class Monitorable:
     def start_receiving_health_checks(self):
-        self._health_checks_receiver_process = mp.Process(target=self.__receive_health_checks)
+        self._health_checks_receiver_process = mp.Process(target=self.__receive_health_checks, daemon=True)
         self._health_checks_receiver_process.start()
         logging.info("action: health_checks_receiver_process_started | result: success")
         
